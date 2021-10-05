5a11c79a87d29b894dba6ced764239bfb2c99dfe
ben <benedikt.maciejczyk@gmx.de>
2021-10-05 12:19:51 +0200

    adjust constructor of todo class

--- src/main/java/com/example/kanban/model/Todo.java
+++ src/main/java/com/example/kanban/model/Todo.java
@@ -1,9 +1,19 @@
 package com.example.kanban.model;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
+import java.util.Objects;
+
+import static com.example.kanban.model.TodoStatus.TODO;
 
 @Entity
 @Table(name = "todos")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +22,23 @@ public class Todo {
     private String title;
     private String description;
     private TodoStatus status;
+
+    public Todo(String title, String description){
+        this.title = title;
+        this.description = description;
+        this.status = TODO;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Todo todo = (Todo) o;
+        return Objects.equals(id, todo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
--- src/main/java/com/example/kanban/service/TodoService.java
+++ src/main/java/com/example/kanban/service/TodoService.java
@@ -1,4 +1,27 @@
 package com.example.kanban.service;
 
+import com.example.kanban.model.Todo;
+import com.example.kanban.repo.TodoRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Data
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepo;
+
+    public List<Todo> getAllTodos(){
+        return List.of(new Todo());
+    }
+    public Todo getSingleTodo(){
+        return new Todo();
+    }
+    public Todo createTodo(String title, String description){
+        return new Todo(title, description);
+    }
+
 }
