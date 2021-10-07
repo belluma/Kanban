76cda3c6cf6faa70f5b12087cfc20185a6a80e1e
ben <benedikt.maciejczyk@gmx.de>
2021-10-07 19:55:13 +0200

    add dependencies for vault testcontainers

--- src/test/java/com/example/kanban/controller/TodoControllerIntegrationTest.java
+++ src/test/java/com/example/kanban/controller/TodoControllerIntegrationTest.java
@@ -1,22 +1,42 @@
 package com.example.kanban.controller;
 
+import com.example.kanban.model.Todo;
+import com.example.kanban.repo.TodoRepository;
+import org.hibernate.tool.schema.spi.SchemaCreator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+
+//source: https://phauer.com/2019/focus-integration-tests-mock-based-tests/
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TodoControllerIntegrationTest {
 
     private JdbcTemplate template;
-    private MockMvc client;
+    private TodoRepository todoRepo;
 
     @BeforeAll
     public void setup() throws IOException {
-        PostgreSQLContainer db
+        PostgreSQLContainer db = new PostgreSQLContainer("postgres:11.2-alpine");
+        db.start();
+        DataSource dataSource = DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .username(db.getUsername())
+                .password(db.getPassword())
+                .url(db.getJdbcUrl())
+                .build();
+        this.template = new JdbcTemplate(dataSource);
+        template.execute("Create Table IF NOT EXISTS products (id varchar(40) primary key, name varchar(40));");
+//        TodoRepository todoRepo = new TodoRepository();
+        TodoController controller = new TodoController();
+
+
     }
 
 
