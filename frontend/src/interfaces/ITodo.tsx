1353e8d464940d3443cdef87445fc4ea670ceeff
ben <benedikt.maciejczyk@gmx.de>
2021-10-08 18:23:10 +0200

    add status enum to interfaces

--- frontend/src/interfaces/ITodo.tsx
+++ frontend/src/interfaces/ITodo.tsx
@@ -1,14 +1,20 @@
 export interface ITodo {
-    id:number,
-    title:string,
-    description:string,
-    created?:Date,
-    deadline?:Date,
+    id: number,
+    title: string,
+    description: string,
+    status: status,
+    created?: Date,
+    deadline?: Date,
 }
 
 export interface ITodoList {
     todo: ITodo[],
     doing: ITodo[],
     done: ITodo[],
+}
 
+export enum status {
+    TODO = "TODO",
+    DOING = "DOING",
+    DONE = "DONE"
 }
